ddd70fd5c9d978bbcfc6b6f9ec7321af0f3454f0
owenli <102705391+VoxelFS@users.noreply.github.com>
2025-01-13 16:51:04 +1300

    init commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				"cgrey": "#454545",
+				"rquartz": "#F7CAC9"
+			}
+		},
+	},
+	plugins: [],
+}
