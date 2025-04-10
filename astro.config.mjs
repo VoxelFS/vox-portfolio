fb9ec05e738c40d8b4a27a0e6638f4fbe670fe21
owenli <102705391+VoxelFS@users.noreply.github.com>
2025-04-10 12:59:08 +1200

    fixed deployment????

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: 'https://voxelfs.github.io',
-  base: 'vox-portfolio'
+  site: 'https://voxelfs.com',
+  base: '/'
 });
