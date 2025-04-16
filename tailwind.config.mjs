65516e82c8d3a7279e5ae11aa6b551432bfae25c
VoxelFS <supicw@gmail.com>
2025-04-17 01:09:54 +1200

    added some themes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,7 +7,11 @@ export default {
 				"cgrey": "#454545",
 				"rquartz": "#F7CAC9",
 				"odark": "#363434",
-				"opink": "#E8C4B8"
+				"opink": "#E8C4B8",
+				"background": 'var(--color-bg)',
+				"text": 'var(--color-text)',
+				"textAccent": 'var(--color-accent-text)',
+				"accent": 'var(--color-accent)'
 			}
 		},
 	},
